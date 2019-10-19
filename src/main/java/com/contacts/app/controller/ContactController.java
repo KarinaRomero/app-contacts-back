834542c9ca136d84a71c8331cb73befd1d1095b1
Karina Romero <betzy_skamm@hotmail.com>
2019-10-19 07:37:01 -0700

    added swagger to documentation

---
+++ src/main/java/com/contacts/app/controller/ContactController.java
@@ -0,0 +1,4 @@
+package com.contacts.app.controller;
+
+public class ContactController {
+}
---
+++ src/main/java/com/contacts/app/util/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.contacts.app.util;
+
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Class to configure the Swagger documentation
+ */
+public class SwaggerConfiguration {
+    public Docket documentation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
